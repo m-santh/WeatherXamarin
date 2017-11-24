ca3c4df4993ff00e3a0f5c0dc4d9bdd693d8acec
Mohit Chhabra <Mohitchhabra@techie.com>
2017-11-24 16:09:11 +0530

    init

---
+++ App7/App7/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Sample Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                if (weather != null)
+                {
+                    this.BindingContext = weather;
+                    getWeatherBtn.Text = "Search Again";
+                }
+            }
+        }
+    }
+}
